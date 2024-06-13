2386befdc05353b4275c4074db06086556d667de
papasgeorge1808 <papasgeorge1808@gmail.com>
2024-06-13 21:56:41 +0300

    Решил задачу

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("John", "Doe", (byte) 50);
+        userService.saveUser("Jane", "Doe", (byte) 24);
+        userService.saveUser("Jack", "Smith", (byte) 44);
+        userService.saveUser("Alex", "Lee", (byte) 26);
+
+        List<User> userList = userService.getAllUsers();
+        System.out.println(userList);
+
+        userService.createUsersTable();
+
+        userService.dropUsersTable();
+
     }
 }
